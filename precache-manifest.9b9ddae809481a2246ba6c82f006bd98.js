28434e8bc766e678795aea43c7c5c5c8b5c0a67b
Artem Bazunts <bazunc@gmail.com>
2019-09-25 11:57:41 +0500

    Updates

---
+++ precache-manifest.9b9ddae809481a2246ba6c82f006bd98.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fcf76c69d0fa1133a2a01f821ef2f17",
+    "url": "/trucksweb/index.html"
+  },
+  {
+    "revision": "77c1d91735b2ba9b1cba",
+    "url": "/trucksweb/static/css/2.ff318fa6.chunk.css"
+  },
+  {
+    "revision": "1cd6fa2fce1a4d9c3b59",
+    "url": "/trucksweb/static/css/main.561401bc.chunk.css"
+  },
+  {
+    "revision": "77c1d91735b2ba9b1cba",
+    "url": "/trucksweb/static/js/2.7241041f.chunk.js"
+  },
+  {
+    "revision": "1cd6fa2fce1a4d9c3b59",
+    "url": "/trucksweb/static/js/main.0e012ecc.chunk.js"
+  },
+  {
+    "revision": "876ab639530fbcb93f2d",
+    "url": "/trucksweb/static/js/runtime~main.0a557f35.js"
+  },
+  {
+    "revision": "27c2806c67b122d4ae841304decf0898",
+    "url": "/trucksweb/static/media/arrow.27c2806c.svg"
+  },
+  {
+    "revision": "1c1c954c80b956dc65cbe6a65347b7df",
+    "url": "/trucksweb/static/media/arrow_black.1c1c954c.svg"
+  },
+  {
+    "revision": "a81d01378e709a017fb9dbecfe2dd221",
+    "url": "/trucksweb/static/media/driver.a81d0137.svg"
+  },
+  {
+    "revision": "3f908cd86eabfb926e30380de6f27fe7",
+    "url": "/trucksweb/static/media/logo.3f908cd8.png"
+  },
+  {
+    "revision": "d3baef83e56a85cc30c3c9dd5a5895e2",
+    "url": "/trucksweb/static/media/next_arrow.d3baef83.svg"
+  },
+  {
+    "revision": "e904a14de5001bb12f825865ec4cf503",
+    "url": "/trucksweb/static/media/request.e904a14d.svg"
+  },
+  {
+    "revision": "04b81ea415add53dfd5c44f5947ed7b7",
+    "url": "/trucksweb/static/media/right-arrow.04b81ea4.svg"
+  },
+  {
+    "revision": "75dc4e8feff301039e9c4c73ac3f29ae",
+    "url": "/trucksweb/static/media/site-logo.75dc4e8f.png"
+  },
+  {
+    "revision": "c6716a39258e2dde34889df422c0c782",
+    "url": "/trucksweb/static/media/truck.c6716a39.svg"
+  }
+]);
